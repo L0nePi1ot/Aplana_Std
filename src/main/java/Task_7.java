6b335e51642f91b3aa3aa288f9aaf346bb4949ef
L0neP1lot <chaeper@gmail.com>
2019-01-15 11:51:24 +0300

    Add Task_7 code

---
+++ src/main/java/Task_7.java
@@ -0,0 +1,11 @@
+public class Task_7 {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+        while (x <= 50) {
+            System.out.print(x + " ");
+            x++;
+        }
+    }
+}
