23227d12d60d8ffe9e28de443517ba222cc08e0f
L0neP1lot <chaeper@gmail.com>
2019-01-15 12:33:28 +0300

    Task_4: Types of vars x,y,z changed to double from int.

--- src/main/java/Task_4.java
+++ src/main/java/Task_4.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class Task_4
-{
+public class Task_4 {
+
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Введите 3 целых числа:");
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        int z = scanner.nextInt();
-        int a = (x + y + z) / 3;
-        System.out.println("Среднее арифметическое число: " + a);
+        System.out.println("Введите 3 числа:");
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        double z = scanner.nextDouble();
+        double a = (x + y + z) / 3;
+        int b = (int)a;
+        System.out.println("Среднее арифметическое число: " + b);
         if ((a / 2) > 3) {
             System.out.println("Программа выполнена корректно");
         }
-
     }
 }
